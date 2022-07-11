014a49518bb51b89d4d03972c067ed2ce730b278
SantiagoAriel17 <107890742+SantiagoAriel17@users.noreply.github.com>
2022-07-11 00:07:52 -0300

    Add files via upload

---
+++ Dump20220709.sql
@@ -0,0 +1,257 @@
+CREATE DATABASE  IF NOT EXISTS `supermark` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `supermark`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: supermark
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `carrito`
+--
+
+DROP TABLE IF EXISTS `carrito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `carrito` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cantidad` int unsigned DEFAULT NULL,
+  `idusuario` int NOT NULL,
+  `idproducto` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idusuario_fk` (`idusuario`),
+  KEY `idproducto_fk` (`idproducto`),
+  CONSTRAINT `idproducto_fk` FOREIGN KEY (`idproducto`) REFERENCES `producto` (`id_producto`),
+  CONSTRAINT `idusuario_fk` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `carrito`
+--
+
+LOCK TABLES `carrito` WRITE;
+/*!40000 ALTER TABLE `carrito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `carrito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comprobante`
+--
+
+DROP TABLE IF EXISTS `comprobante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comprobante` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `total` float DEFAULT NULL,
+  `tipo` varchar(1) DEFAULT NULL,
+  `fecha` timestamp NULL DEFAULT NULL,
+  `destinatario` int NOT NULL,
+  `idtransferencia` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `destinatario_fk` (`destinatario`),
+  KEY `idtranserencia_fk` (`idtransferencia`),
+  CONSTRAINT `destinatario_fk` FOREIGN KEY (`destinatario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `idtranserencia_fk` FOREIGN KEY (`idtransferencia`) REFERENCES `transferencia` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comprobante`
+--
+
+LOCK TABLES `comprobante` WRITE;
+/*!40000 ALTER TABLE `comprobante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comprobante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalleventa`
+--
+
+DROP TABLE IF EXISTS `detalleventa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalleventa` (
+  `iddetalle_venta` int NOT NULL,
+  `idventa` int NOT NULL,
+  `idarticulo` int NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) NOT NULL,
+  PRIMARY KEY (`iddetalle_venta`),
+  KEY `comprobante_fk` (`idventa`),
+  KEY `producto_fk` (`idarticulo`),
+  CONSTRAINT `comprobante_fk` FOREIGN KEY (`idventa`) REFERENCES `comprobante` (`id`),
+  CONSTRAINT `producto_fk` FOREIGN KEY (`idarticulo`) REFERENCES `producto` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalleventa`
+--
+
+LOCK TABLES `detalleventa` WRITE;
+/*!40000 ALTER TABLE `detalleventa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalleventa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `domicilio`
+--
+
+DROP TABLE IF EXISTS `domicilio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `domicilio` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `barrio` varchar(30) DEFAULT NULL,
+  `calle` varchar(30) DEFAULT NULL,
+  `entre_calle1` varchar(30) DEFAULT NULL,
+  `entre_calle2` varchar(30) DEFAULT NULL,
+  `numero` int unsigned DEFAULT NULL,
+  `piso` int unsigned DEFAULT NULL,
+  `dpto` varchar(10) DEFAULT NULL,
+  `instructivo_de_entrega` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domicilio`
+--
+
+LOCK TABLES `domicilio` WRITE;
+/*!40000 ALTER TABLE `domicilio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domicilio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `factura` (
+  `id_factura` int NOT NULL AUTO_INCREMENT,
+  `idcomprobante` int NOT NULL,
+  `empleado` varchar(30) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`id_factura`),
+  KEY `idcomprobante_fk` (`idcomprobante`),
+  CONSTRAINT `idcomprobante_fk` FOREIGN KEY (`idcomprobante`) REFERENCES `comprobante` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factura`
+--
+
+LOCK TABLES `factura` WRITE;
+/*!40000 ALTER TABLE `factura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `producto` (
+  `id_producto` int NOT NULL AUTO_INCREMENT,
+  `Nombre_Producto` varchar(30) DEFAULT NULL,
+  `Precio_Producto` int unsigned DEFAULT NULL,
+  `Cantidad_Producto` int unsigned DEFAULT NULL,
+  `Marca_Producto` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transferencia`
+--
+
+DROP TABLE IF EXISTS `transferencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transferencia` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cbu` decimal(20,0) NOT NULL,
+  `banco` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transferencia`
+--
+
+LOCK TABLES `transferencia` WRITE;
+/*!40000 ALTER TABLE `transferencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transferencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `dni` int unsigned NOT NULL,
+  `EMAIL` varchar(40) NOT NULL,
+  `contrasenia` varchar(16) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `iddomicilio` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `iddomicilio_fk` (`iddomicilio`),
+  CONSTRAINT `iddomicilio_fk` FOREIGN KEY (`iddomicilio`) REFERENCES `domicilio` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-09 19:11:25
